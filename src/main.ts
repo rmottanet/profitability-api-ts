725e62d25beb5802b2c6c234454e59cc258b7262
Rodrigo Motta <rmotta.info@gmail.com>
2023-12-25 18:54:39 -0300

    oups

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,7 @@ import * as TaxasService from './services/taxasService.ts';
 const app = new Application();
 
 app.use(oakCors({
-	origin: "https://rmottanet.github.io/profitability/";
+	origin: "https://rmottanet.github.io/profitability/"
 }));
 
 app.use(async (ctx, next) => {
